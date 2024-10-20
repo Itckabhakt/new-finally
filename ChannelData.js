31ee4343c5050ef267c724e2f50930048ad46585
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-10-20 23:21:32 +0530

    Create ChannelData.js

---
+++ ChannelData.js
@@ -0,0 +1,74 @@
+const channelData = {
+  "Usa": {
+    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "252a671825ba31ec8433f978c32ccf6d",
+        "key": "ee560759ecc8d2274df2e63fcef56915"
+      }
+    }
+  },
+  "Wilo1": {
+    "file": "https://cors-proxy.cooks.fyi/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLHD-3291.dfw.1080)/index.m3u8",
+    "drm": {
+      "clearkey": {
+        "keyId": "6a63735cef825eaa950bf10a48717f4b",
+        "key": "cf6b5b32f2a55d0b0bd685b6e8e4b084"
+      }
+    }
+  },
+  "Wilo2": {
+    "file": "https://zerotoone.com.br/wp-content/plugins/super-links/application/helpers/super-links-proxy.php/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLHD-3291.dfw.1080)/index.m3u8",
+    "drm": {
+      "clearkey": {
+        "keyId": "252a671825ba31ec8433f978c32ccf6d",
+        "key": "ee560759ecc8d2274df2e63fcef56915"
+      }
+    }
+  },
+  "Xtr1": {
+    "file": "https://cors-proxy.cooks.fyi/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8",
+    "drm": {
+      "clearkey": {
+        "keyId": "6a63735cef825eaa950bf10a48717f4b",
+        "key": "cf6b5b32f2a55d0b0bd685b6e8e4b084"
+      }
+    }
+  },
+  "Xtr2": {
+    "file": "https://zerotoone.com.br/wp-content/plugins/super-links/application/helpers/super-links-proxy.php/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8",
+    "drm": {
+      "clearkey": {
+        "keyId": "252a671825ba31ec8433f978c32ccf6d",
+        "key": "ee560759ecc8d2274df2e63fcef56915"
+      }
+    }
+  },
+  "Tusa": {
+    "file": "https://fsly.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(kvea)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "dfb59142ce523a6c900758d5ee4c7997",
+        "key": "bd2d684e590815190eda056a6d9618bc"
+      }
+    }
+  },
+  "NBC1": {
+    "file": "https://fsly.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(knbc)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0045a118e231f1326bcdb45350b1ceaa",
+        "key": "8c13afbfa54ea37a368b8b859021f6e3"
+      }
+    }
+  },
+  "NBC2": {
+    "file": "https://fsly.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(universo-east)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "232d71bb013d39ffb92ee8c576fb7e8e",
+        "key": "087408a48be8d57046c3868b37ea90ba"
+      }
+    }
+  }
+};
