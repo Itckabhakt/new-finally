5281b1bca68b75381c6b2f97e75fa387086049af
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-10-20 23:25:20 +0530

    Update ChannelData.js

--- ChannelData.js
+++ ChannelData.js
@@ -12,8 +12,8 @@ const channelData = {
     "file": "https://cors-proxy.cooks.fyi/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLHD-3291.dfw.1080)/index.m3u8",
     "drm": {
       "clearkey": {
-        "keyId": "6a63735cef825eaa950bf10a48717f4b",
-        "key": "cf6b5b32f2a55d0b0bd685b6e8e4b084"
+        "keyId": "59ed8dc9d4a48857ef8e5865919496cc",
+        "key": "0d3680bd8b349deda8b7fdd1da71b7f8"
       }
     }
   },
@@ -21,8 +21,8 @@ const channelData = {
     "file": "https://zerotoone.com.br/wp-content/plugins/super-links/application/helpers/super-links-proxy.php/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLHD-3291.dfw.1080)/index.m3u8",
     "drm": {
       "clearkey": {
-        "keyId": "252a671825ba31ec8433f978c32ccf6d",
-        "key": "ee560759ecc8d2274df2e63fcef56915"
+        "keyId": "59ed8dc9d4a48857ef8e5865919496cc",
+        "key": "0d3680bd8b349deda8b7fdd1da71b7f8"
       }
     }
   },
@@ -30,8 +30,8 @@ const channelData = {
     "file": "https://cors-proxy.cooks.fyi/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8",
     "drm": {
       "clearkey": {
-        "keyId": "6a63735cef825eaa950bf10a48717f4b",
-        "key": "cf6b5b32f2a55d0b0bd685b6e8e4b084"
+        "keyId": "1f52934d28a25c51a5dd577a650dcece",
+        "key": "323a46b02a31172c2475406e56fd3be6"
       }
     }
   },
@@ -39,8 +39,8 @@ const channelData = {
     "file": "https://zerotoone.com.br/wp-content/plugins/super-links/application/helpers/super-links-proxy.php/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8",
     "drm": {
       "clearkey": {
-        "keyId": "252a671825ba31ec8433f978c32ccf6d",
-        "key": "ee560759ecc8d2274df2e63fcef56915"
+        "keyId": "1f52934d28a25c51a5dd577a650dcece",
+        "key": "323a46b02a31172c2475406e56fd3be6"
       }
     }
   },
