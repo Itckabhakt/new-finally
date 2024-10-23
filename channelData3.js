687b3164a15029d9449004290cad8cd33f2793e1
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-10-23 13:49:38 +0530

    Create channelData3.js

---
+++ channelData3.js
@@ -0,0 +1,11 @@
+const channelData = {
+    "Ten": {
+        "file": "https://edge3-moblive.yuppcdn.net/drm/smil:tencricketdrm.smil/manifest.mpd", 
+        "drm": {
+            "clearkey": {
+                "keyId": "abfc480ae7920a3ebd9e99e7b9a0d3e2",
+                "key": "63a7fe4e7899ba2bc5bbd4307398f3ff" 
+            }
+        }
+    },
+  };
