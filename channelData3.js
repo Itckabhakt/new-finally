dc213eb528e885f07832c20865adebe846973689
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-11-08 21:08:35 +0530

    Update channelData3.js

--- channelData3.js
+++ channelData3.js
@@ -1,10 +1,10 @@
 const channelData = {
-    "astrocricket": {
-        "file": "https://finallympd.vercel.app/2504/default.mpd", 
+    "cricbuzz": {
+        "file": "https://live2.shoq.com.pk/live/eds/Criclife2/DASH/Criclife2.mpd", 
         "drm": {
             "clearkey": {
-                "keyId": "03c2e0af2f8159f9f0ce9b5dbc865f10",
-                "key": "cd84ed136b0cc71f8ab8cd4d4f6a2e4c" 
+                "keyId": "4301796d6d67374043c4a43c18dff7ea",
+                "key": "96a3dc8766317aa169149a604928ebb6" 
             }
         }
     },
