6428c41bdcdedf6bc15bf6e58585127b5c4d8c0f
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2025-01-14 19:38:57 +0530

    Update channelData3.js

--- channelData3.js
+++ channelData3.js
@@ -35,4 +35,13 @@ const channelData = {
             }
         }
     },
+    "newtra": {
+        "file": "https://cors-anywhere.ammoapps.com/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8", 
+        "drm": {
+            "clearkey": {
+                "keyId": "1f52934d28a25c51a5dd577a650dcece",
+                "key": "323a46b02a31172c2475406e56fd3be6" 
+            }
+        }
+    },
   };
